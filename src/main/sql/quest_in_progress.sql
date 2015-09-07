c530e45c863e4ae97559a90a99db6fbac65cebc4
dev-null-matt <dev.null.matt@gmail.com>
2015-09-06 22:45:17 -0500

    Adds new columns for start/completion date of QIP

--- src/main/sql/quest_in_progress.sql
+++ src/main/sql/quest_in_progress.sql
@@ -4,11 +4,13 @@
 create sequence quest_in_progress_id;
 
 create table quest_in_progress (
-	id				integer PRIMARY KEY default nextval('quest_in_progress_id'),
-	character_id	integer NOT NULL REFERENCES character(id),
-	quest_id		integer NOT NULL REFERENCES quest(id),
-	current_step	integer NOT NULL default 1,
-	tracked			boolean NOT NULL default false
+	id							integer PRIMARY KEY default nextval('quest_in_progress_id'),
+	character_id		integer NOT NULL REFERENCES character(id),
+	quest_id				integer NOT NULL REFERENCES quest(id),
+	current_step		integer NOT NULL default 1,
+	tracked					boolean NOT NULL default false,
+	start_date			timestamp NOT NULL default localtimestamp,
+	completed_date	timestamp
 );
 
 insert into quest_in_progress (character_id,quest_id) values (1,1);
