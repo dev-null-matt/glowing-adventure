204d9eff750c611ca95dbd79d40bc0b468cfc275
dev-null-matt <dev.null.matt@gmail.com>
2015-09-24 23:59:42 -0500

    Replaces database system config with config file

--- src/main/java/com/arrested/lbmmo/util/SystemSettings.java
+++ src/main/java/com/arrested/lbmmo/util/SystemSettings.java
@@ -2,16 +2,22 @@ package com.arrested.lbmmo.util;
 
 public enum SystemSettings {
 
-	ACCOUNT_MAX_CHARACTERS("ACCOUNT.MAX_CHARACTERS"),
-	CHARACTER_MAX_QUESTS("CHARACER.MAX_QUESTS");
+	ACCOUNT_MAX_CHARACTERS("ACCOUNT.MAX_CHARACTERS", Integer.class),
+	CHARACTER_MAX_QUESTS("CHARACER.MAX_QUESTS", Boolean.class);
 	
 	private String settingName;
+	private Class<? extends Object> type;
 	
-	private SystemSettings(String settingName) {
+	private SystemSettings(String settingName, Class<? extends Object> type) {
 		this.settingName = settingName;
+		this.type = type;
 	}
 	
 	public String getSettingName() {
 		return settingName;
+	}
+	
+	public Class<? extends Object> getType() {
+		return type;
 	}
 }
